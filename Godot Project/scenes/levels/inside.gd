07131d91d3ba3ccc3dd4afbccf147688d199dde6
Alek-Hanusic <22imc10240@fh-krems.ac.at>
2023-12-16 00:30:12 +0100

    More UI and transitionLayer work

--- Godot Project/scenes/levels/inside.gd
+++ Godot Project/scenes/levels/inside.gd
@@ -7,9 +7,12 @@ extends LevelParent
 func change_scene(scene_path: String) -> void:
 	get_tree().change_scene_to_file(scene_path)
 	#get_tree().change_scene_to_packed(outside_level_scene)
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
 
 
 func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	call_deferred("change_scene", "res://scenes/levels/outside.tscn")
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
+	#call_deferred("change_scene", "res://scenes/levels/outside.tscn")
+	
